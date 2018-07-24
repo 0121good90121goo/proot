92ab743ee1db944c5a29514d5118763a60e68cca
Matt Ferrero <mattaferrero@gmail.com>
2018-07-24 16:43:19 -0400

    handle function test

--- src/extension/fake_id0/TEST_HANDLE_OPEN.c
+++
@@ -1,5 +0,0 @@
-int main()
-{
-	handle_open(0, 0, 0, 0, 0, 0);
-}
-
---
+++ src/extension/fake_id0/handle_functions.h
@@ -0,0 +1,3 @@
+int handle_open(Tracee *tracee, Reg fd_sysarg, Reg path_sysarg,       
+    Reg flags_sysarg, Reg mode_sysarg, Config *config);
+
