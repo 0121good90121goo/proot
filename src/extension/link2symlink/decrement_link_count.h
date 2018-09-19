050c2f5c83e8ad4397c982f7383e4e2f08c9dba4
Matt Ferrero <mattaferrero@gmail.com>
2018-09-19 06:06:31 -0400

    .. conflicting dirs?

--- src/extension/link2symlink/decrement_link_count.h
+++
@@ -1,8 +0,0 @@
-#ifndef DECREMENT_LINK_COUNT_H_
-#define DECREMENT_LINK_COUNT_H_
-
-#include "tracee/reg.h"
-
-extern int decrement_link_count(Tracee *tracee, Reg sysarg);
-
-#endif
--- src/extension/link2symlink/my_readlink.h
+++
@@ -1,8 +0,0 @@
-#ifndef MY_READLINK_H_
-#define MY_READLINK_H_
-
-#include <linux/limits.h>
-
-extern int my_readlink(const char symlink[PATH_MAX], char value[PATH_MAX]);
-
-#endif
